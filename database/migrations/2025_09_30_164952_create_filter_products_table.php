9ef508cac9d4ad4e4818b46a44cd18a304569b55
Parham <37364144+Makav3li94@users.noreply.github.com>
2025-10-26 12:13:15 +0330

    first on server

--- database/migrations/2025_09_30_164952_create_filter_products_table.php
+++ database/migrations/2025_09_30_164952_create_filter_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->ulid('product_id')->index();
             $table->ulid('filter_id')->index();
             $table->string('value');
-            $table->foreign('product_id')->references('id')->on('product_id')
+            $table->foreign('product_id')->references('id')->on('products')
                 ->onDelete('cascade');
             $table->foreign('filter_id')->references('id')->on('filters')
                 ->onDelete('cascade');
