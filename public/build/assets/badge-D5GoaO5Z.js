a0f7778e444fad7be0192164c4fb216522bd65c4
Parham <37364144+Makav3li94@users.noreply.github.com>
2025-10-27 09:09:19 +0330

    phone adddres

--- public/build/assets/badge-D5GoaO5Z.js
+++
@@ -1,6 +0,0 @@
-import{c as r}from"./createLucideIcon-Bo3pdkBr.js";import{j as s}from"./app-D0BZgUMB.js";/**
- * @license lucide-react v0.536.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]],i=r("pencil",t);function l(e){const{type:a,outline:c}=e,n={neutral:"badge-primary",primary:"badge-primary",secondary:"badge-secondary",accent:"badge-accent",info:"badge-info",error:"badge-error",success:"badge-success",warning:"badge-warning",only:""};return s.jsx("div",{className:`badge ${c?"badge-outline":""} ${a?n[a]:n.default} ${e.className?e.className:""}`,children:e.children})}export{l as B,i as P};
